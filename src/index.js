520718d07a37e069c96078aed7d2942f7145babb
Maysam <maysam82.dev@gmail.com>
2020-03-29 21:05:58 +1100

    setup redux for app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
 import App from './Containers/App/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+import balanceReducer from './balance/balance';
+export default rootReducer = combineReducers({
+	balanceReducer,
+});
