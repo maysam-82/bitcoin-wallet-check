eb337518d5e55134e5d76553b3994b117594fbbc
Maysam <maysam82.dev@gmail.com>
2020-03-30 14:54:42 +1100

    pass test for adding Wallet component to App

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Wallet from '../../Components/Wallet/Wallet';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<h2>Money Check</h2>
+				<Wallet />
 			</div>
 		);
 	}
--- src/Containers/App/App.spec.js
+++ src/Containers/App/App.spec.js
@@ -7,4 +7,7 @@ describe('App Component', () => {
 	it('should render App properly', () => {
 		expect(wrapper.debug()).toMatchSnapshot();
 	});
+	it('should render `Connect(Wallet)` component', () => {
+		expect(wrapper.find('Connect(Wallet)').exists()).toBe(true);
+	});
 });
