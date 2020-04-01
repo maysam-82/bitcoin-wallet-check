6c77829ee7cb0d4ccdd326f5d06d0881910b8107
Maysam <maysam82.dev@gmail.com>
2020-04-01 21:53:53 +1100

    pass local state for wallet

--- src/Components/Wallet/Wallet.js
+++ src/Components/Wallet/Wallet.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 export class Wallet extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			moneyAmount: 0,
+		};
+	}
+	onMoneyChanged = ({ target: { value } }) => {
+		const moneyAmount = parseInt(value);
+		this.setState({ moneyAmount });
+	};
 	render() {
 		const { balance } = this.props;
 		return (
 			<div>
 				<h2 className="balance">Money Balance: {balance}</h2>
+				<input className="input-money" onChange={this.onMoneyChanged} />
 			</div>
 		);
 	}
--- src/Components/Wallet/Wallet.test.js
+++ src/Components/Wallet/Wallet.test.js
@@ -11,4 +11,16 @@ describe('Wallet Component', () => {
 	it('should display the balance equal to 20', () => {
 		expect(wrapper.find('.balance').text()).toEqual('Money Balance: 20');
 	});
+	it('should create an input to deposite into or withdraw from the balance', () => {
+		expect(wrapper.find('.input-money').exists()).toBe(true);
+	});
+	describe('while entering an amount of money in input', () => {
+		const amountOfMoney = '45	';
+		beforeEach(() => {
+			wrapper.find('.input-money').simulate('change', { target: { value: amountOfMoney } });
+		});
+		it('should update `moneyAmount` in `state`', () => {
+			expect(wrapper.state().moneyAmount).toEqual(parseInt(amountOfMoney));
+		});
+	});
 });
