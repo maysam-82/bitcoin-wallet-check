b44c11a6e98810ef0c28901a1e22469a8735e439
Maysam <maysam82.dev@gmail.com>
2020-04-05 22:04:08 +1000

    change class comp to stateless

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Wallet from '../../Components/Wallet/Wallet';
 import Bitcoin from '../../Components/Bitcoin/Bitcoin';
 
-class App extends Component {
-	render() {
-		return (
-			<div>
-				<h2>Money Check</h2>
-				<Wallet />
-				<Bitcoin />
-			</div>
-		);
-	}
-}
+const App = () => {
+	return (
+		<div>
+			<h2>Money Check</h2>
+			<Wallet />
+			<Bitcoin />
+		</div>
+	);
+};
 
 export default App;
